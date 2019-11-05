8869f1f1c8f9e86934bbf1799f5a7f273fbc8db1
Lussfer <niravr1995@gmail.com>
2019-11-05 14:27:39 -0600

    relationship has been setup + seeds populated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  validates :title, presence: true
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 end
