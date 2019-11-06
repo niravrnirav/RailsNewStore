9cf1b15eccda0d3a7cab70b24346e3470a5475ee
Lussfer <niravr1995@gmail.com>
2019-11-05 19:34:13 -0600

    adding shopping cart functionality

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,9 @@
 class OrderItemsController < ApplicationController
 
+  def index
+    @items = current_cart.order.items
+  end
+
   def create
     current_cart.add_item(
       product_id: params[:product_id],
--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -5,16 +5,19 @@ class ShoppingCart
   end
 
   def order
-    #if order is assigned, it will return that instance. if it is null it will create a new order
-    @order ||= Order.find_or_create_by(token) do |order|
+    @order ||= Order.find_or_create_by(token: @token) do |order|
       order.sub_total = 0
     end
+  end
+
+  def items_count
+    order.items.sum(:quantity)
   end
 
   def add_item(product_id:, quantity: 1)
     product = Product.find(product_id)
 
-    order_item = order.items.find_or_create_by(
+    order_item = order.items.find_or_initialize_by(
       product_id: product_id
     )
 
@@ -24,4 +27,35 @@ class ShoppingCart
     order_item.save
   end
 
+  def remove_item(id:)
+    order.items.destroy(id)
+  end
+
 end
+# class ShoppingCart
+
+#   def initialize(token:)
+#     @token = token
+#   end
+
+#   def order
+#     #if order is assigned, it will return that instance. if it is null it will create a new order
+#     @order ||= Order.find_or_create_by(token: @token) do |order|
+#       order.sub_total = 0
+#     end
+#   end
+
+#   def add_item(product_id:, quantity: 1)
+#     product = Product.find(product_id)
+
+#     order_item = order.items.find_or_create_by(
+#       product_id: product_id
+#     )
+
+#     order_item.price = product.price
+#     order_item.quantity = quantity
+
+#     order_item.save
+#   end
+
+# end
