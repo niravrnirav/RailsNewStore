94dc74f7490d88e1e7d0598d0215572aaf0c32ff
Lussfer <niravr1995@gmail.com>
2019-11-05 19:17:06 -0600

    added current cart in application controller, uses session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+  before_action :current_cart
+
+  end
+
+  def current_cart
+    @current_cart ||= ShoppingCart.new(token: cart_token)
+  end
+  helper_method :current_cart
+
+  private
+
+  def cart_token
+    return @cart_token unless @cart_token.nil?
+
+    #if new session, assign new hash
+    session[:cart_token] ||= SecureRandom.hex(8)
+    @cart_token = session[:cart_token]
+  end
 end
