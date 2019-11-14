9e2172e6349a374a743db9798f0dc1f284e43c21
Lussfer <niravr1995@gmail.com>
2019-11-13 21:08:02 -0600

    added new products + recently updated products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,14 @@ class ProductsController < ApplicationController
   end
 
   def show_all_products
-    @query = params[:query].downcase
+    @query = params[:query]
+
+    @query = params[:query].downcase unless @query.nil?
 
     @all_products = Product.where('lower(title) LIKE ?', "%#{@query}%").order(:title).page params[:page]
+    @new_prod = Product.first(3)
+    last_updated_products = Product.order(updated_at: :desc)
+    @recent_updates = last_updated_products.first(3)
 
     if !params[:category_id].nil? && params[:category_id] != '89'
       category = Category.find(params[:category_id])
