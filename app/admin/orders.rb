1e61ba64f777c244335ac2cc16c045a478f3ce71
Lussfer <niravr1995@gmail.com>
2019-11-14 23:18:31 -0600

    frozen thing

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,11 +1,12 @@
-ActiveAdmin.register Order do
+# frozen_string_literal: true
 
+ActiveAdmin.register Order do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :first_name, :last_name, :sub_total, :token, :status
+  permit_params :first_name, :last_name, :sub_total, :token, :status
   #
   # or
   #
@@ -14,5 +15,4 @@ ActiveAdmin.register Order do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
   has_many :items, class_name: 'OrderItem'
 end
