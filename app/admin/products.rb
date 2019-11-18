0b583cb1f87f42b568f8ca657af4952f29a7e239
Lussfer <niravr1995@gmail.com>
2019-11-18 10:31:11 -0600

    removed active storage form

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -8,14 +8,14 @@ ActiveAdmin.register Product do
   #
   permit_params :title, :description, :price, :stock_quantity, :image
 
-  form do |f|
-    f.semantic_errors
-    f.inputs
-    f.inputs do
-      f.input :image, as: :file
-    end
-    f.actions
-  end
+  # form do |f|
+  #   f.semantic_errors
+  #   f.inputs
+  #   f.inputs do
+  #     f.input :image, as: :file
+  #   end
+  #   f.actions
+  # end
 
   #
   # or
