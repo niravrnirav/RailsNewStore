106f449577bdd6836f007345df51496b4f5ba9ec
Lussfer <niravr1995@gmail.com>
2019-11-21 14:53:55 -0600

    created province table

---
+++ db/migrate/20191121205139_create_provinces.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateProvinces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :provinces do |t|
+      t.string :name
+      t.decimal :tax
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_162813) do
+ActiveRecord::Schema.define(version: 2019_11_21_205139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,13 @@ ActiveRecord::Schema.define(version: 2019_11_15_162813) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "stock_quantity"
     t.string "image"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.decimal "tax"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
