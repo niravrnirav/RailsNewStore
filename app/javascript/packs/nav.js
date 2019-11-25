f0c3521e6ee3f9affb81efb9c3b93115f556d597
Lussfer <niravr1995@gmail.com>
2019-11-25 15:33:17 -0600

    tried fixing vue

--- app/javascript/packs/nav.js
+++ app/javascript/packs/nav.js
@@ -1,12 +1,12 @@
-import Vue from 'vue/dist/vue.js';
-import TurbolinksAdapter from 'vue-turbolinks';
-Vue.use(TurbolinksAdapter)
+import Vue from "vue/dist/vue.js";
+import TurbolinksAdapter from "vue-turbolinks";
+Vue.use(TurbolinksAdapter);
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener("turbolinks:load", () => {
     new Vue({
-        el: '#app',
+        el: "#app",
         data: {
             showNav: false
         }
     });
-})
+});
